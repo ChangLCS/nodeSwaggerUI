e113575d989e976829c236f46e78134db8a08cbf
Chang <584911924@qq.com>
2018-03-20 22:58:55 +0800

    开了个本地html页面，还有server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const http = require('http');
+const url = require('url');
+
+const data = {
+  name: 'Chang',
+  age: 26,
+  birthday: '1992-12-29',
+};
+
+const server = http.createServer((req, res) => {
+  const urlInfo = url.parse(req.url, true);
+  if (urlInfo.path === '/test') {
+    res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
+
+    res.end(JSON.stringify(data));
+  }
+});
+
+server.listen(9000);
+
+console.log('-----------------------------');
+console.log('server running');
